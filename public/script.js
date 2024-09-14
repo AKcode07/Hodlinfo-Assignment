650ce8549d1d0bf2c7f8f74a11228077555dd4ed
AKcode07 <ashwinkumavat07@gmail.com>
2024-09-15 00:07:21 +0530

    Designed User Interface

--- public/script.js
+++ public/script.js
@@ -18,11 +18,47 @@ document.addEventListener("DOMContentLoaded", () => {
           <td>₹ ${Number(ticker.buy).toLocaleString()} / ₹ ${Number(
           ticker.sell
         ).toLocaleString()}</td>
+          <td>${ticker.volume} %</td>
           <td>${ticker.volume}</td>
-          <td>${ticker.base_unit}</td>
         `;
         tbody.appendChild(row);
       });
     })
     .catch((error) => console.error("Error fetching data:", error));
 });
+
+const spinner = document.getElementById("spinner");
+const timerLabel = document.getElementById("timer-label");
+let totalTime = 60;
+let timeLeft = totalTime;
+
+function updateSpinner() {
+  const percentage = (timeLeft / totalTime) * 100;
+  spinner.style.setProperty("--percentage", percentage);
+  timerLabel.textContent = timeLeft;
+
+  if (timeLeft > 0) {
+    timeLeft--;
+  } else {
+    timeLeft = totalTime;
+  }
+}
+
+let timerInterval = setInterval(updateSpinner, 1000);
+
+const themeToggle = document.getElementById("theme-toggle");
+const toggleLabel = document.getElementById("toggle-label");
+const body = document.body;
+
+themeToggle.addEventListener("change", () => {
+  if (themeToggle.checked) {
+    // If the toggle is checked, apply dark theme
+    body.classList.add("dark-theme");
+    toggleLabel.classList.add("dark-theme-toggle");
+  } else {
+    // If the toggle is unchecked, revert to light theme
+    body.classList.remove("dark-theme");
+    toggleLabel.classList.remove("dark-theme-toggle");
+  }
+});
+
